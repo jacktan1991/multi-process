cbb5fc696336876de5d0a2507f83e87c2a535deb
tanchen <tanchen01@baidu.com>
2019-12-22 19:00:42 +0800

    feat:python:python版fork子进程

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # multi-process
+
+## Python版
+
+### Charpt 1
+python 中 fork 子进程基本写法，代码运行的基本流程调试信息
+
+参考：
+- https://www.geeksforgeeks.org/creating-child-process-using-fork-python
+- https://stackoverflow.com/questions/33560802/pythonhow-os-fork-works
+- https://www.meccanismocomplesso.org/en/multiprocessing-in-python-forking-a-process
---
+++ python/ch_1_simple.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# coding:utf8
+
+import os
+
+def main():
+    print("main start...")
+
+    pid = os.fork()
+    print("this point for child process is 'resume'-ing running, while for parent procss is 'continue'-ing running")
+    if pid == 0:
+        # Codes running in the child process eventually.
+        print("%d (child) just was created by %d." % (os.getpid(), os.getppid()))
+    else:
+        # Codes running in the parent process eventually.
+        print("%d (parent) just created %d." % (os.getpid(), pid))
+
+    print("main end...")
+
+if __name__ == "__main__":
+    main()
